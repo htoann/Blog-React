dfd281072d509eb19a13ff738d0f7912a594e256
Trần Hữu Toàn <jungleoffice02@gmail.com>
2021-06-09 23:55:21 +0700

    front-end test

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import "./Home.css";
+import BlogList from "./BlogList";
+import { useState } from "react";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs}></BlogList>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,30 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>HToan Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">Create A New Blog</a>
+        <a href="/profile">Messages</a>
+        <a class="profile" href="/profile">
+          User
+          <div className="auth">
+            <ul>
+              <li>
+                <a href="/auth/logout">Log out</a>
+              </li>
+              <li>
+                <a href="/auth/signup">Sign up</a>
+              </li>
+            </ul>
+          </div>
+        </a>
+        <a href="/auth/login">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
