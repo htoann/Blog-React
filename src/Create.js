e5d96b16578db45449286312454062c249edfc04
Trần Hữu Toàn <jungleoffice02@gmail.com>
2021-06-11 23:53:04 +0700

    input blog

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,45 @@
+import "./assets/BlogCreate.css";
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
     <div className="create">
-      <h2>Add A New Blog</h2>
+      <h2>Write A New Blog</h2>
+      <form>
+        <label>Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label>Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+        <label>Author</label>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        />
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
